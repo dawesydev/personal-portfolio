ce5510f5d88f2927fd7a1c18a3274643ebf741e5
Dan <danieldawes@protonmail.com>
2023-01-11 12:39:16 +0000

    font size alterations

--- components/Divider.vue
+++ components/Divider.vue
@@ -5,22 +5,14 @@
 </script>
 
 <template>
-    <div class="py-12 border border-2 rounded-xl mx-4 sm:mx-8 flex flex-col items-center">
-<!--      <div class="relative">-->
-<!--        <h3 class="text-parchment text-4xl md:text-6xl font-display uppercase place-self-center">-->
-<!--          {{ title }}-->
-<!--        </h3>-->
-<!--        <div class="absolute -top-7 -left-16 md:-left-28">-->
-<!--          <p class="font-marker text-3xl md:text-4xl lg:pb-2 text-emerald-400 translate-x-1 -rotate-12">check out my</p>-->
-<!--        </div>-->
-
-        <p class="font-marker text-4xl lg:text-6xl lg:pb-2 text-emerald-400">check out my</p>
-        <h3 class="text-parchment text-5xl md:text-7xl font-display uppercase place-self-center">
+    <div class="border rounded-xl p-4 sm:p-8 mx-4 sm:mx-8 grid place-items-center text-center">
+      <div class="text-parchment font-display uppercase">
+        <span class="font-marker text-emerald-400 text-2xl md:text-4xl">check out my</span>
+        <br />
+        <span class="text-4xl md:text-6xl">
           {{ title }}
-        </h3>
-
-<!--      </div>-->
-
+        </span>
+      </div>
   </div>
 </template>
 
