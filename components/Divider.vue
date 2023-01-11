f9548a28e160b08f48a240b0355d1a9fd917ac51
Dan <danieldawes@protonmail.com>
2023-01-11 12:25:47 +0000

    divider updated

--- components/Divider.vue
+++ components/Divider.vue
@@ -5,15 +5,21 @@
 </script>
 
 <template>
-    <div class="pt-12 pb-10 border border-2 rounded-xl mx-4 sm:mx-8 flex justify-center items-center">
-      <div class="relative">
-        <h3 class=" text-parchment text-4xl md:text-6xl font-display uppercase place-self-center">
+    <div class="py-12 border border-2 rounded-xl mx-4 sm:mx-8 flex flex-col items-center">
+<!--      <div class="relative">-->
+<!--        <h3 class="text-parchment text-4xl md:text-6xl font-display uppercase place-self-center">-->
+<!--          {{ title }}-->
+<!--        </h3>-->
+<!--        <div class="absolute -top-7 -left-16 md:-left-28">-->
+<!--          <p class="font-marker text-3xl md:text-4xl lg:pb-2 text-emerald-400 translate-x-1 -rotate-12">check out my</p>-->
+<!--        </div>-->
+
+        <p class="font-marker text-4xl lg:text-6xl lg:pb-2 text-emerald-400">check out my</p>
+        <h3 class="text-parchment text-5xl md:text-7xl font-display uppercase place-self-center">
           {{ title }}
         </h3>
-        <div class="absolute -top-7 -left-16 md:-left-28">
-          <p class="font-marker text-3xl md:text-4xl lg:pb-2 text-emerald-400 translate-x-1 -rotate-12">check out my</p>
-        </div>
-      </div>
+
+<!--      </div>-->
 
   </div>
 </template>
