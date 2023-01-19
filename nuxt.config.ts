9bf522f7e5a2a21d3370f61fe148cbab350025dc
Dan <danieldawes@protonmail.com>
2023-01-19 17:48:12 +0000

    meta added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-16',
+            viewport: 'width=500, initial-scale=1',
+            title: 'Daniel Dawes',
+            meta: [
+                // <meta name="description" content="My amazing site">
+                { name: 'description', content: 'Daniel Dawes is a freelance web developer. Bringing ideas to life through code.' }
+            ],
+        }
+    },
     modules: [
         'nuxt-icon',
     ],
