9ac1560f4c0993feba6725e899f3dc16ec839848
Dan <danieldawes@protonmail.com>
2023-01-19 17:50:12 +0000

    minor updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
         head: {
             charset: 'utf-16',
             viewport: 'width=500, initial-scale=1',
-            title: 'Daniel Dawes',
+            title: 'Daniel Dawes | Frontend Developer',
             meta: [
                 // <meta name="description" content="My amazing site">
                 { name: 'description', content: 'Daniel Dawes is a freelance web developer. Bringing ideas to life through code.' }
